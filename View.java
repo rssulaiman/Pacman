d9c1fb79e08afa6049630fb6be52c2ccffebf535
rssulaiman <76789581+rssulaiman@users.noreply.github.com>
2021-12-14 21:58:03 +0100

    Update View.java

--- View.java
+++ View.java
@@ -10,13 +10,14 @@ public class View extends JFrame{
 	public View (Panel fenetre) {
 		this.fenetre = fenetre;	
 		
+		// Le pacman et le background apparaissent parfaitement mais le pacman ne bouge pas, il ne bouge que si on commente les lignes 17, 18 et uncommente la ligne 20
 		JFrame frame = new JFrame("PAC-MAN");
 		Jeu jeu = new Jeu();
 		frame.add(jeu);
-		//frame.add(fenetre);
-		//frame.setSize(fenetre.getSize());
+		frame.add(fenetre);
+		frame.setSize(fenetre.getSize());
 		frame.setResizable(false);
-		frame.setSize(800, 500);
+		//frame.setSize(800, 500);
 		frame.setLocation(500, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
