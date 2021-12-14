e1947d7858d61d760a8970959ca40be726fb8a7a
rssulaiman <76789581+rssulaiman@users.noreply.github.com>
2021-12-14 19:03:10 +0100

    Create Panel.java

---
+++ Panel.java
@@ -0,0 +1,41 @@
+package pacman;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+
+import javax.swing.JComponent;
+
+
+
+public class Panel extends JComponent{
+
+	private final Jeu game;
+
+
+
+	private static final long serialVersionUID = 1L;
+
+	
+	public Panel (Jeu game) {
+		super();
+		this.game = game;
+		setOpaque(true);
+		setSize( game.getCaseSize()*game.getCaseNumWidth(), game.getCaseSize()*game.getCaseNumHeight());
+		}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+	g.setColor(Color.black);
+       g.fillRect(0, 0, game.getCaseSize()*game.getCaseNumWidth(),game.getCaseSize()*game.getCaseNumHeight());
+    Graphics2D g2d = (Graphics2D) g;
+    
+     game.createMap(g2d);
+   game.paintComponent(g2d);
+   
+     }
+	
+	
+}
